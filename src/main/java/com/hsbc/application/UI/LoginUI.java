c211ac5a3c9243925d825fc58bc6eae6f6fff4e7
Vedant Dambhare <94474290+VedantDambhare@users.noreply.github.com>
2024-08-23 11:05:43 +0530

    Adding Login Functionality

---
+++ src/main/java/com/hsbc/application/UI/LoginUI.java
@@ -0,0 +1,58 @@
+package com.hsbc.application.UI;
+
+import com.hsbc.application.dao.AdminDAO;
+import com.hsbc.application.dao.LoginDao;
+import com.hsbc.application.daoimpl.AdminDAOImpl;
+import com.hsbc.application.daoimpl.LoginDaoImpl;
+import com.hsbc.application.exceptions.UserNotFoundException;
+
+import java.util.Scanner;
+
+public class LoginUI {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean flag = true;
+        while(flag) {
+            System.out.println("Enter your username: ");
+            String username = sc.nextLine();
+            System.out.println("Enter your password: ");
+            String password = sc.nextLine();
+            LoginDao login = new LoginDaoImpl();
+
+            try {
+                String s = login.authenticate(username, password);
+                System.out.println("Welcome " + username+ " logging in as "+s);
+                //redirect to UI
+
+                switch (s){
+                    case "PROJECT_MANAGER":
+                        System.out.println("Redirecting to PROJECT_MANAGER page...");
+                        flag = false;
+//                        AdminUI adminUi = new AdminUI();
+                        break;
+                    case "DEVELOPER":
+                        System.out.println("Redirecting to DEVELOPER page...");
+                        flag = false;
+                        break;
+                    case "TESTER":
+                        System.out.println("Redirecting to TESTER page...");
+                        flag = false;
+                        break;
+                    default:
+                        System.out.println("Invalid username or password");
+                        break;
+                }
+            } catch (UserNotFoundException e) {
+                System.out.println("No such user found!");
+                System.out.println("Please re-enter credentials");
+            } finally {
+                continue;
+            }
+        }
+
+
+        sc.close();
+    }
+}
